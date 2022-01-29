8d57441fe471543f9ac273d709530b272a2d72b0
GowthamSakthi <gowtham17693@gmail.com>
2022-01-29 19:56:42 +0530

    project structures

---
+++ src/org/test/GitTest.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import org.junit.Test;
+
+public class GitTest {
+@Test
+	private void test() {
+	
+	System.out.println("test1");
+	System.out.println("test2");
+	System.out.println("test3");
+		
+
+	}
+	
+	
+	
+}
